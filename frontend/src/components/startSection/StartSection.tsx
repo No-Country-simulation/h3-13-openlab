2fe47bae4c7bea7f8c88857b2be1d6b476c446b7
Abel Acuña <ale_lasarte@hotmail.com>
2024-12-01 23:07:18 -0300

    Add landing mobile changes

--- frontend/src/components/startSection/StartSection.tsx
+++ frontend/src/components/startSection/StartSection.tsx
@@ -2,23 +2,27 @@ import { startData } from "../../utils/startSectionData";
 
 const StartSection = () => {
   return (
-    <section className="text-color-0 h-[600px] flex flex-col items-center gap-20 my-20">
-      <div className="flex gap-5 w-[1400px] items-center flex-col">
-        <p className="text-5xl font-medium">¡Comienza ahora!</p>
-        <p className="text-lg font-light max-w-[520px] text-center">
+    <section className="text-color-0 min-h-[600px] flex flex-col items-center gap-20 my-20">
+      <div className="container flex flex-col items-center gap-5">
+        <p className="text-3xl font-medium md:text-5xl">¡Comienza ahora!</p>
+        <p className="md:text-lg text-base font-light md:max-w-[520px] max-w-[300px] text-center">
           Ayuda a diseñar, construir y promover a las startups del futuro:
           abiertas, descentralizadas y transparentes.
         </p>
       </div>
-      <div className="flex gap-5 w-[1400px] items-start justify-evenly">
+      <div className="container flex flex-col items-center gap-5 md:items-start md:flex-row justify-evenly">
         {startData.map((user, index) => (
           <div
-            className="flex flex-col items-center max-w-[280px] gap-4 p-5"
+            className="flex md:flex-col items-center md:max-w-[280px] gap-4 p-5"
             key={index}
           >
             <img src={user.icon} alt="icon" className="w-24 h-24" />
-            <p className="text-lg font-semibold">{user.title}</p>
-            <p className="text-base text-center">{user.description}</p>
+            <div className="flex flex-col gap-3 md:items-center">
+              <p className="text-lg font-semibold md:text-center">
+                {user.title}
+              </p>
+              <p className="text-base md:text-center">{user.description}</p>
+            </div>
           </div>
         ))}
       </div>
